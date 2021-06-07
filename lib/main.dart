9076c4b5eb6d193d429a4ed7a6b4fb5c8040d2f6
Rajarshi Samaddar <itsrajarshi@gmail.com>
2021-06-07 21:10:51 +0530

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,270 +10,151 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+class MyTheme {
+  static final darkTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.black,
+  );
+}
+
 class _MyAppState extends State<MyApp> {
-  int i = 0, sum = 0;
-  double s = 0.0;
-  String t = '';
+  String output = "0";
 
-  void numberm() {
+  String _output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
+
+  buttonPressed(String buttonText) {
+    if (buttonText == "CLEAR") {
+      _output = "0";
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "/" ||
+        buttonText == "X") {
+      num1 = double.parse(output);
+
+      operand = buttonText;
+
+      _output = "0";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        return;
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "EQUAL'S  (=)") {
+      num2 = double.parse(output);
+
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+      }
+      if (operand == "X") {
+        _output = (num1 * num2).toString();
+      }
+      if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+
+    print(_output);
+
     setState(() {
-      i = int.parse(t);
-      s = s - i;
-      t = t + '-';
+      output = double.parse(_output).toStringAsFixed(2);
     });
   }
 
-  void numberp() {
-    setState(() {
-      i = int.parse(t);
-      s = s + i;
-      t = t + '+';
-    });
-  }
-
-  void numberd() {
-    setState(() {
-      i = int.parse(t);
-      s = s / i;
-      t = t + '/';
-    });
-  }
-
-  void numbermu() {
-    setState(() {
-      i = int.parse(t);
-      s = s * i;
-      t = t + '*';
-    });
-  }
-
-  void numbere() {
-    setState(() {
-      t = t + '=' + s.toString();
-    });
-  }
-
-  void number9() {
-    setState(() {
-      t = t + '9';
-    });
-  }
-
-  void number8() {
-    setState(() {
-      t = t + '8';
-    });
-  }
-
-  void number7() {
-    setState(() {
-      t = t + '7';
-    });
-  }
-
-  void number6() {
-    setState(() {
-      t = t + '6';
-    });
-  }
-
-  void number5() {
-    setState(() {
-      t = t + '5';
-    });
-  }
-
-  void number4() {
-    setState(() {
-      t = t + '4';
-    });
-  }
-
-  void number3() {
-    setState(() {
-      t = t + '3';
-    });
-  }
-
-  void number2() {
-    setState(() {
-      t = t + '2';
-    });
-  }
-
-  void number1() {
-    setState(() {
-      t = t + '1';
-    });
-  }
-
-  void number0() {
-    setState(() {
-      s = s * 10 + 0;
-      t = t + '0';
-    });
+  Widget buildButton(String buttonText) {
+    return new Expanded(
+      child: new OutlineButton(
+        padding: new EdgeInsets.all(24.0),
+        child: new Text(
+          buttonText,
+          style: GoogleFonts.lato(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.orange[900]),
+        ),
+        onPressed: () => buttonPressed(buttonText),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('C A L C U L A T O R'),
-        ),
-        body: Column(
-          children: [
-            Padding(padding: EdgeInsets.all(120)),
-            Text(
-              t,
-              style: TextStyle(fontSize: 30),
-              textAlign: TextAlign.center,
-            ),
-            Padding(padding: EdgeInsets.all(60)),
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(4)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('C'),
-                    onPressed: () => print('one')),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('+ -'),
-                    onPressed: () => print('o')),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('%'),
-                    onPressed: () => print('one')),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.orangeAccent,
-                    textColor: Colors.white,
-                    child: Text('/'),
-                    onPressed: numberd),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(4)),
-                RaisedButton(
-                  color: Colors.blueAccent,
-                  textColor: Colors.white,
-                  child: Text('7'),
-                  onPressed: number7,
+        themeMode: ThemeMode.dark,
+        darkTheme: MyTheme.darkTheme,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('ignorecrowd.co.in ©',
+                style: GoogleFonts.lato(
+                  fontSize: 38.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                )),
+            backgroundColor: Colors.orange[900],
+          ),
+          body: new Container(
+            child: new Column(
+              children: <Widget>[
+                new Container(
+                    alignment: Alignment.centerRight,
+                    padding: new EdgeInsets.symmetric(
+                        vertical: 24.0, horizontal: 12.0),
+                    child: new Text(output,
+                        style: GoogleFonts.lato(
+                          fontSize: 48.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white60,
+                        ))),
+                new Expanded(
+                  child: new Divider(),
                 ),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('8'),
-                    onPressed: number8),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('9'),
-                    onPressed: number9),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.orangeAccent,
-                    textColor: Colors.white,
-                    child: Text('*'),
-                    onPressed: numbermu),
+                new Column(children: [
+                  new Row(children: [
+                    buildButton("7"),
+                    buildButton("8"),
+                    buildButton("9"),
+                    buildButton("/")
+                  ]),
+                  new Row(children: [
+                    buildButton("4"),
+                    buildButton("5"),
+                    buildButton("6"),
+                    buildButton("X")
+                  ]),
+                  new Row(children: [
+                    buildButton("1"),
+                    buildButton("2"),
+                    buildButton("3"),
+                    buildButton("-")
+                  ]),
+                  new Row(children: [
+                    buildButton("."),
+                    buildButton("0"),
+                    buildButton("00"),
+                    buildButton("+")
+                  ]),
+                  new Row(children: [
+                    buildButton("CLEAR"),
+                    buildButton("EQUAL'S  (=)"),
+                  ])
+                ])
               ],
             ),
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(4)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('4'),
-                    onPressed: number4),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('5'),
-                    onPressed: number5),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('6'),
-                    onPressed: number6),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.orangeAccent,
-                    textColor: Colors.white,
-                    child: Text('-'),
-                    onPressed: numberm),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(4)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('1'),
-                    onPressed: number1),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('2'),
-                    onPressed: number2),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('3'),
-                    onPressed: number3),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.orangeAccent,
-                    textColor: Colors.white,
-                    child: Text('+'),
-                    onPressed: numberp),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(4)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('00'),
-                    onPressed: () => print('one')),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('0'),
-                    onPressed: number0),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.blueAccent,
-                    textColor: Colors.white,
-                    child: Text('.'),
-                    onPressed: () => print('one')),
-                Padding(padding: EdgeInsets.all(7)),
-                RaisedButton(
-                    color: Colors.purple,
-                    textColor: Colors.white,
-                    child: Text('='),
-                    onPressed: numbere),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
